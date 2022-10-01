99f77e82cd4bd84494d7671f8a4f16f101f65028
notAperson535 <notAperson939@gmail.com>
2022-10-01 08:47:56 -0400

    Tab closing and more settings stuff with @YanMaker (broton on discord)

--- public/tabs.js
+++ public/tabs.js
@@ -2,17 +2,32 @@ var nextNumber = 1
 var zIndex = 10
 let tabsection = document.getElementsByClassName("tabsection")[0]
 let body = document.querySelector("body")
+var tabOrder = new Array();
+var goOrNot = 1
 
 createTabAndIframe();
 
-function openTab(evt, tabNumber) {
-  document.querySelector("#" + tabNumber.replace("iframe","") + " img").addEventListener("click", ()=>{
-    document.querySelector("#" + tabNumber).outerHTML = ""
-    document.querySelector("#" + tabNumber.replace("iframe","")).outerHTML = ""
-    return;
-  })
+function openTab(tabNumber) {
+  var tab = document.querySelector("#" + tabNumber.replace("iframe",""))
+  var tabimg = document.querySelector("#" + tabNumber.replace("iframe","") + " img")
 
-  if (typeof(document.querySelector(".iframe.active")) != 'undefined' && document.querySelector(".iframe.active") != null)
+  if(tabimg.getAttribute("listener") !== "true"){
+    tabimg.addEventListener("click", ()=>{
+      document.querySelector("#" + tabNumber).outerHTML = ""
+      tab.outerHTML = ""
+      tabOrder.splice(tabOrder.indexOf(tabNumber), 1);
+      openTab(tabOrder.slice(-1)[0])
+    })
+  }
+
+  tabimg.setAttribute("listener", "true")
+
+  if (tabOrder.indexOf(tabNumber) > -1) {
+    tabOrder.splice(tabOrder.indexOf(tabNumber), 1);
+  }
+  tabOrder[tabOrder.length] = tabNumber;
+
+  if (typeof(document.querySelector(".iframe.active")) != "undefined" && document.querySelector(".iframe.active") != null)
   {
     document.querySelector(".iframe.active").style.display = "none"
     document.querySelector(".iframe.active").classList.remove("active")
@@ -33,7 +48,7 @@ function openTab(evt, tabNumber) {
     }else{
       document.querySelector("#urlbarhomepage input").value = ""
     }
-    document.getElementById(tabNumber.replace("iframe","")).className += " active";
+    tab.className += " active";
 }
 
 function createTabAndIframe(){
@@ -41,7 +56,7 @@ function createTabAndIframe(){
     tabdiv.classList.add("tab");
     tabdiv.id = "tab" + nextNumber
     tabsection.appendChild(tabdiv);
-    tabdiv.setAttribute("onclick", "openTab(event, 'tab" + nextNumber + "iframe')")
+    tabdiv.setAttribute("onclick", "openTab('tab" + nextNumber + "iframe')")
 
     tabdivp = document.createElement("p");
     tabdivp.innerHTML = "Tab"
@@ -51,13 +66,14 @@ function createTabAndIframe(){
     tabdivimg.src = "closetab.png"
     tabdivimg.classList.add("invert");
     tabdiv.appendChild(tabdivimg);
+    tabdivimg.setAttribute("listener", "false");
 
     iframe = document.createElement("iframe");
     iframe.classList.add("iframe");
     iframe.id = "tab" + nextNumber + "iframe"
     body.appendChild(iframe);
 
-    openTab(event, "tab" + nextNumber + "iframe");
+    openTab("tab" + nextNumber + "iframe");
 
     nextNumber = nextNumber + 1
 }
