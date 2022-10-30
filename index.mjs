1b950d76298f65dc84805afd927089e8dd86f15b
830008 <55303069+830008@users.noreply.github.com>
2022-10-30 13:16:04 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,8 +1,15 @@
 import Server from 'bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
-
-
+import cluster from 'cluster';
+import os from 'os';
+const numCPUs = os.cpus().length;
+if(cluster.isMaster){
+console.log("Running");
+	for(let i = 0; i < numCPUs; i++){
+	cluster.fork();
+	}
+}else{
 const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('public/');
 
@@ -18,3 +25,4 @@ server.on('upgrade', (req, socket, head) => {
 	socket.end();
 });
 server.listen(process.env.PORT || 80, () => {console.log("Abyss running at http://localhost:80")});
+}
