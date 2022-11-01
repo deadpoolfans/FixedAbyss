5024a4178b1059095db3a6b11b99d8789cfd1aba
830008 <55303069+830008@users.noreply.github.com>
2022-10-31 20:43:21 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: '/about/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
